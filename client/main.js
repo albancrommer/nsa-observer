93d06619b6cdd074096fd867ca93a07277d41e69
alban <alban@octopuce.fr>
2014-01-23 01:26:29 +0100

    [enh] wiki edit proto

--- client/main.js
+++ client/main.js
@@ -14,7 +14,181 @@ var  transformWikiLinks = function(string) {
   }
   return str_res;
 }
+var Item = function(name){
 
+        var instance = {};
+	instance.name			= name;
+	instance.category 		= "";
+	instance.relatedItems           = [];
+	instance.family			= "";
+	instance.status			= "";
+	instance.links			= [];
+	instance.currentState	= null;
+	
+	instance.__construct = function( name ){
+		instance.name = name;
+	}
+	instance.getInsertString = function(){
+            str = 'Items.insert({name:"'+instance.get("name")+'",category:"'+instance.get("category")+'",family:"'+instance.get("family")+'",status:"'+instance.get("status")+'",description:"'+instance.get("description")+'",relatedItems:'+instance.get("relatedItems", TRUE)+',links:'+instance.get("links", TRUE)+'});';
+            return str.replace("\n","","g")+"\n";
+	}
+	instance.get = function(name,json){
+            if( json){
+                return JSON.stringify(instance.name);
+            }
+            return addslashes(instance.name);
+	}
+        return instance;
+}
+
+var itemParser = function(){
+    var 
+        instance = {},
+        itemList = [],
+        currentItem = null
+    ;
+    instance.getItemList = function(){
+        return itemList;
+    }
+    
+    instance.read = function(row){
+        
+        var it = 0,
+            regexp = null,
+            callback = function(){},
+            validator = function(){},
+            tuple = [],
+            res = [],
+            expressions = [
+            [/^== ([A-Z]) ==$/,function(r){
+                instance.addItem()
+            }],
+            [/=== +(.+?) +===/,function(r){
+                instance.addItem()
+                currentItem = new Item(r[1]);
+            }],
+            [/""Short *?Description"" *?: *?(.*?) *?\n$/,function(r){
+                currentItem.description = trim($matches[1]);	
+                currentItem.currentState = "description";
+            }],
+            [/""Category"" *: *(.*?)$/,function(r){
+
+			if( "" == r[1].trim()){
+				r[1] = "program";
+			};
+			currentItem.category = r[1].trim();	
+			currentItem.currentState = null;
+            }],
+            [/""Family"" *?: *?(.*?)$/,function(r){
+                if( "" == r[1].trim()){
+                                    r[1] = "collect";
+                            };
+                            currentItem.family = r[1].trim();	
+                            currentItem.currentState = null;
+            }],
+            [/""Related items"" *?: *?(.*?)$/,function(r){
+                currentItem.relatedItems = r[1].trim().split(" ");
+                currentItem.currentState = null;
+            }],
+            [/""Status"" *?: *?(.*?)$/,function(r){
+			if( "" == (r[1])){
+				r[1] = "unknown";
+			};
+			currentItem.status = r[1].trim();	
+			currentItem.currentState = null;                
+            }],
+            [/""Links"" :/,function(r){
+                currentItem.currentState = "links";
+            }],
+            [/""Links"" :/,function(r){
+                currentItem.currentState = "links";
+            }],
+            [/\[(.*)\|(.*)\]/,function(r){
+                if( ! r[1] ){
+                    instance.log( "[invalid url]"+ row);
+                    return;
+                }
+                url 	= r[1];
+                label	= r[2] ? r[2] : url;
+                currentItem.links.push([url,label]); 
+            },function(){currentItem.currentState == "links" ? true:false;}],
+            [null,function(r){
+                if( currentItem.currentState != "links" && "" != row && ! is_null(currentItem) && ! is_null(currentItem.currentState )){
+                        currentItem[currentItem.currentState] += row;
+                }
+            }],
+            [null,function(){
+                instance.log( "[not parsed]"+ row);
+            }]
+        ];
+        for (it = 0; it < expressions.length; it++) {
+            tuple = expressions[it];
+            regexp = tuple[0];
+            callback = tuple[1];
+            validator = tuple[2];
+            if ( null === regexp) {
+                if( ! validator ){
+                    break;
+                }
+            }
+            res = regexp.exec(row);
+            if( res && res.length > 0){
+                callback( res );
+                return;
+            }
+        }
+    }
+    /**
+     * 
+     */
+    instance.log = function(str){
+        console.log(str);
+    };
+    
+    /**
+     * 
+     */
+    instance.addItem = function(){
+        if( !currentItem){
+            return;
+        }
+        var item = currentItem, name = currentItem.name, list = itemList;
+        if( ! name ){
+            // todo : log
+            instance.log("[err] could not retrieve name for "+JSON.stringify(currentItem));
+        }
+        itemList.push(item);
+        list = itemList;
+    }
+    
+    /**
+     * 
+     * @param {type} doc
+     * @returns {@exp;instance@call;getItemList}
+     */
+    instance.run = function(doc){
+        var rowList = doc.split("\n");
+        for(var i = 0; i < rowList.length; i++){
+            var row = rowList[i].trim();
+            if( row ){
+                instance.read(row);
+            }
+        }
+        instance.addItem();
+        return itemList;
+    }
+    
+    return instance;
+    
+}
+
+window.itemParser = new itemParser();
+
+
+var parseWikiItems = function(text){
+
+    
+}
 var itemLinkShowList = function(event){
     var 
         element     = $(event.currentTarget),
@@ -72,6 +246,13 @@ if( ! (Session.get("sliderRendered") ) ){
 if( ! (Session.get("showNav") ) ){
     Session.set("showNav",false);
 }
+if( ! (Session.get("itemEdit") ) ){
+    Session.set("itemEdit",false);
+}
+
+if( ! (Session.get("editModeType") ) ){
+    Session.set("editModeType","wiki");
+}
 
 
 
@@ -120,8 +301,10 @@ Template.item.events({
 
 Template.itemShow.currentItem = function(){
     var item = Session.get("currentItem");
-    item.relatedItems = $.map(item.relatedItems,function(x,y){return transformWikiLinks(x)})
-    item.description = transformWikiLinks(item.description);
+    if( !Session.get("editMode")){
+        item.relatedItems = $.map(item.relatedItems,function(x,y){return transformWikiLinks(x)})
+        item.description = transformWikiLinks(item.description);
+    }
     return item;
 }
 
@@ -143,20 +326,69 @@ Template.itemShow.events({
     },
     'click .family-link':itemLinkShowList,
     'click .category-link':itemLinkShowList,
+    'click .edit-mode':function(e){
+        Session.set("editMode",true);
+    },
+    'click .view-mode':function(e){
+        Session.set("editMode",false);
+    },
+    'click .item-save':function(e){
+        var data
+        if( Session.get('editModeType') === "wiki"){
+            data = $(".wiki-item-container").html();
+            console.log(data);
+        }else{
+            // @todo
+            var elements = $(".item-data");
+            console.log(elements.each(function(x,y){console.log( $(y).val())}))
+        }
+    }
 });
 
-
+Template.itemShow.canEdit= function(){
+    return Meteor.user() || false;
+}
+Template.itemShow.inEditMode = function(){
+    return Session.get("editMode");
+}
 Template.itemShow.externalLink = function(l){
     return '<li><a href="'+l[0]+'" target="_blank" class="item-show-link external-link">'+l[1]+'</a></li>';
-
 }
+Template.itemShow.inEditModeWiki = function() {
+    return "wiki" === Session.get('editModeType');
+}
+Template.itemShow.editExternalLink = function(){
+    var txt = "";
+    $(Session.get("currentItem").externalLinks).each(function(x,y){txt += y[0]+"|"+y[1];});
+    return txt;
+}
+Template.itemShow.editRelatedItems = function(l){
+    var txt = "";
+    $(Session.get("currentItem").relatedItems).each(function(x,y){txt += y+" ";});
+    return txt;
+}
+
+
+// itemWiki
+
+Template.itemWiki.editWikiRelatedItems = function(l){
+    var txt = " ";
+    $(Session.get("currentItem").relatedItems).each(function(x,y){if (y) txt += "[["+y+"]] ";});
+    return txt;
+}
+Template.itemWiki.editWikiExternalLink = function(){
+    var txt = "";
+    console.log($(Session.get("currentItem").externalLinks))
+    $(Session.get("currentItem").links).each(function(x,y){if (y) txt += "* ["+y[0]+"|"+y[1]+"]\n";});
+    return txt;
+}
+
 
 // nav
 
 Template.nav.showNav = function(){
     return Session.get("showNav");
 }
-
 
 Template.nav.events({
     'click a':itemLinkShowList,
@@ -177,13 +409,11 @@ Template.nav.events({
     }
 })
 
-
 // Slider
 
 Template.slider.events({
     'click a':itemLinkShowList,
     'click circle':itemLinkShowList,
-            
 })
 
 Template.slider.num = function(){
