9eb6b48b22e6627bf73e407795cce08fa8ec4a4b
alban <alban@octopuce.fr>
2014-01-25 23:40:48 +0100

    [enh] tweaks visual content

--- client/main.js
+++ client/main.js
@@ -439,6 +439,10 @@ Template.slider.events({
     'click .export-wiki':function(){
         Session.set('showExport', true);
         Session.set('exportType', 'wiki');
+    },
+    'click .export-json':function(){
+        Session.set('showExport', true);
+        Session.set('exportType', 'json');
     }
 })
 
@@ -512,8 +516,25 @@ Template.export.showExport = function(){
 Template.export.itemList = function(){
     return  Items.find({},{order:{name:1}}).fetch();
 };
+Template.export.JSONitemList = function(){
+    return  JSON.stringify(Items.find({},{order:{name:1}}).fetch());
+};
+Template.export.wikiType = function(){
+    return Session.equals("exportType","wiki");
+}
 Template.export.events({
     "click .export-close":function(){
         Session.set('showExport', false);
+    },
+    'click .export-wiki':function(){
+        Session.set('exportType', 'wiki');
+    },
+    'click .export-json':function(){
+        Session.set('exportType', 'json');
     }
 });
+Template.export.rendered = function(){
+    $('.visible-content')
+    .html($(".export-content").html())
+    .select();
+};
