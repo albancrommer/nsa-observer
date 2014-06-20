7055f01dd6c6d32929e5c99611d4985f7d560321
alban <alban@octopuce.fr>
2014-06-20 22:55:22 +0200

    [fix] setgid / setuid as root forbidden

--- server/main.js
+++ server/main.js
@@ -1,10 +1,7 @@
 // Makes sure we don't run as root 
-
-if( 0 === parseInt(process.env.SUDO_UID) ){
 	try{
 		process.setgid("node");
 		process.setuid("node");
 	} catch (err){
 		console.log("failed to setgid / setuid");
 	}
-}
