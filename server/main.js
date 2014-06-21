e0e720c9a92a585321e3e529b72f7599345fbd7e
alban <alban@octopuce.fr>
2014-06-21 20:27:48 +0200

    [fix] server smtp

--- server/main.js
+++ server/main.js
@@ -1,16 +1,21 @@
 // In your server code: define a method that the client can call
 Meteor.methods({
   sendEmail: function (from, to, subject, text) {
-    check([from, to, subject, text], [String]);
+    check([from, subject, text], [String]);
 
     // Let other method calls from the same client start running,
     // without waiting for the email sending to complete.
     // this.unblock();
      Email.send({
-      to: to,
+      to: "admin@nsa-observer.net",
       from: from,
       subject: subject,
       text: text
     });
   }
 });
+// Sets the environnement function
+Meteor.startup(function () {
+    process.env.MAIL_URL = "smtp://localhost"
+});
+
