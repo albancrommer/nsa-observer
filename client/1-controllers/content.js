1407aa509b43ba48765ce563ae5f26432dfd589d
alban <alban@octopuce.fr>
2015-02-11 23:26:40 +0100

    [enh] home & tweaks

--- client/1-controllers/content.js
+++ client/1-controllers/content.js
@@ -1,8 +1,11 @@
 
 Template.content.rendered = function(){
+    
+    $("input.search").focus();
+    
     // esc
     $(document).keyup( function(e) {
-        if (e.keyCode == 27) { 
+        if (e.keyCode === 27) { 
             Router.go("home");
         }   
     })
--- client/1-controllers/home.js
+++ client/1-controllers/home.js
@@ -5,9 +5,13 @@
 Template.home.events({
     'click #home-search-btn': function(e){
         Router.go("search", { name:$("#home-search").val() } );
-        
+    },
+    'keypress #home-search': function(e){
+        // If return pressed
+        if(e.charCode === 13){
+        Router.go("search", { name:$("#home-search").val() } );
+        }
     }
-       
 })
 
 Template.home.isAdmin = function(){
@@ -63,7 +67,6 @@ Template.home.fullTagList = function(){
     }); 
     var final  = sortable.sort(function(a,b){return a.tags > b.tags })
 //    fullList.sort()
-    console.log(sortable)
     return final;
 }
 Template.home.tags_num = function(){
@@ -76,41 +79,8 @@ Template.home.programList = function(){
 Template.home.attackList = function(){
     return Template.home.getCategoryList("attack vector");
 }
-//Template.home.rendered = function(){
-//    
-//    var data;
-//    //    NodeList.drawGraph();
-//    data = [
-////        {c:"undefineds",n:"undefined",q:Items.find({category:"undefined"}).count(),cat:"undefined"},
-//        {c:"programs",n:"programs",q:Items.find({category:"program"}).count(),cat:"program"}, 
-//        {c:"attacks",n:"attack vectors",q:Items.find({category:"attack vector"}).count(),cat:"attack vector"}, 
-//        {c:"compartments",n:"compartments",q:Items.find({category:"compartment"}).count(),cat:"compartment"}
-//    ];
-//    var catGraph = new CategoryGraph();
-//    catGraph.drawGraph("#categories-graph",data,600,100);
-//    
-//    data = [
-//        {c:"collect",n:"collect programs",q:Items.find({category:"program",family:"collect"}).count(),cat:"program",fam:"collect"},
-//        {c:"process",n:"process programs",q:Items.find({category:"program",family:"process"}).count(),cat:"program",fam:"process"}, 
-//        {c:"database",n:"database programs",q:Items.find({category:"program",family:"database"}).count(),cat:"program",fam:"database"}, 
-//        {c:"target",n:"target programs",q:Items.find({category:"program",family:"target"}).count(),cat:"program",fam:"target"}, 
-//        {c:"attack",n:"attack programs",q:Items.find({category:"program",family:"attack"}).count(),cat:"program",fam:"attack"}
-//    ];
-//    var programGraph = new CategoryGraph();
-//    programGraph.drawGraph("#programs-graph",data,600,200);
-//    
-//    data = [
-//        {c:"hardware",n:"hardware attack vectors",q:Items.find({category:"attack vector",family:"hardware"}).count(),cat:"attack vector",fam:"hardware"},
-//        {c:"software",n:"software attack vectors",q:Items.find({category:"attack vector",family:"software"}).count(),cat:"attack vector",fam:"software"}, 
-//        {c:"network",n:"network attack vectors",q:Items.find({category:"attack vector",family:"network"}).count(),cat:"attack vector",fam:"network"} 
-//    ];
-//    var programGraph = new CategoryGraph();
-//    programGraph.drawGraph("#attack-graph",data,600,200);
-//    
-//    data = [
-//        {c:"eci",n:"Extremely Compartmented Information",q:Items.find({category:"compartiment",family:"ECI"}).count(),cat:"compartiment",fam:"ECI"}
-//    ];
-//    var programGraph = new CategoryGraph();
-//    programGraph.drawGraph("#compartment-graph",data,600,200);
-//
-//};
+
+Template.home.rendered = function(){
+    $("#home-search").focus();
+
+}
