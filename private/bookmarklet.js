6ff34981d4bb90b6c967ba06d5ad3f1526628835
alban <alban@octopuce.fr>
2015-02-22 19:10:21 +0100

    [fix] bookmarklet

--- private/bookmarklet.js
+++ private/bookmarklet.js
@@ -1,9 +1,26 @@
-// javascript:(function(){!function(){function n(n){var e=[];o.forEach(function(t){var a=t[1],r=!1;n.forEach(function(n){var i=new RegExp("\\b"+n+"\\b","i"),o=i.exec(a);o&&o.length&&(-1===e.indexOf(n)&&e.push(n),c.push([n,t[0]]),r=!0)}),r&&(t[0].innerHTML=t[0].innerText)});var t=encodeURI(JSON.stringify(e));s=new XMLHttpRequest,s.open("GET",i+"/api/search?q="+t),s.onreadystatechange=function(){4===s.readyState&&200===s.status&&r(JSON.parse(s.response))},s.send()}function e(n,e){return'<a style="display:inline-block; background:#00ccff;color:#fff;" href="'+e+'"> -@NSAO '+n+"- </a> "}function t(n,t){var a=t.innerHTML,r=n.name,o=a.replace(new RegExp("\\b"+r+"\\b","i"),e(r,i+"/"+r));t.innerHTML=o,console.log(t,"old",a,"new",o)}function a(n,t){var a=t.innerHTML,r=n.name,o=a.replace(new RegExp("\\b"+r+"\\b","i"),e(r,i+"/tags/"+r));t.innerHTML=o}function r(n){var e=n.itemList,r=n.tagList;c.forEach(function(n){var i=n[0],o=n[1];i in e?t(e[i],o):a(r[i],o)})}var i="https://localhost:3001",o=[],c=[];["p","li","h1","h2","h3","h4"].forEach(function(n){for(var e=document.getElementsByTagName(n),t=0;t<e.length;t++)o.push([e[t],e[t].innerText])});var s=new XMLHttpRequest;s.open("GET",i+"/api/list"),s.onreadystatechange=function(){4===s.readyState&&200===s.status&&n(JSON.parse(s.response))},s.send()}(window,void 0);})();
+// javascript:(function(){!function(e,n){function%20t(e){return%20e?p?e.innerText:e.textContent:""}function%20a(e){var%20n=[];f.forEach(function(a){var%20r=a[1],i=!1;e.forEach(function(e){var%20t=new%20RegExp("\\b"+e+"\\b","i"),o=t.exec(r);o&&o.length&&(-1===n.indexOf(e)&&n.push(e),u.push([e,a[0]]),i=!0)}),i&&(a[0].innerHTML=t(a[0]))});var%20a=encodeURI(JSON.stringify(n));h=new%20XMLHttpRequest,h.open("GET",c+"/api/search?q="+a),h.onreadystatechange=function(){4===h.readyState&&200===h.status&&s(JSON.parse(h.response))},h.send()}function%20r(e,n){return'<a%20style="display:inline-block;%20background:#00ccff;color:#fff;%20padding:%200.38em%200.61em;"%20href="'+n+'">%20#'+e+"%20</a>%20"}function%20i(e,n){var%20t=n.innerHTML,a=e.name,i=t.replace(new%20RegExp("\\b"+a+"\\b","i"),r(a,c+"/"+a));n.innerHTML=i}function%20o(e,n){var%20t=n.innerHTML,a=e.name,i=t.replace(new%20RegExp("\\b"+a+"\\b","i"),r(a,c+"/tags/"+a));n.innerHTML=i}function%20s(e){var%20n=e.itemList,t=e.tagList;u.forEach(function(e){var%20a=e[0],r=e[1];a%20in%20n?i(n[a],r):o(t[a],r)})}var%20c="https://www.nsa-observer.net",f=[],u=[],p=document.getElementsByTagName("body")[0].innerText!=n?!0:!1;["p","li","h1","h2","h3","h4"].forEach(function(e){for(var%20n=document.getElementsByTagName(e),a=0;a<n.length;a++)f.push([n[a],t(n[a])])});var%20h=new%20XMLHttpRequest;h.open("GET",c+"/api/list"),h.onreadystatechange=function(){4===h.readyState&&200===h.status&&a(JSON.parse(h.response))},h.send()}(window,void%200);})()
 (function (window, undefined) {
 //    var host = "http://localhost:3001";
     var host = "https://www.nsa-observer.net";
     var htmlElementList = [];
     var toBindList = [];
+
+    var hasInnerText = (document.getElementsByTagName("body")[0].innerText != undefined) ? true : false;
+
+    /**
+     * 
+     * @param {type} element
+     * @returns String
+     */
+    function getInnerText(element) {
+        if( ! element ){
+            return "";
+        }
+        if (hasInnerText) {
+            return element.innerText;
+        }
+        return element.textContent;
+    }
 
     /* retrieve html elements and their content*/
     ["p", "li", "h1", "h2", "h3", "h4"].forEach(function (k) {
@@ -11,7 +28,7 @@
         for (var i = 0;
                 i < list.length;
                 i++) {
-            htmlElementList.push([list[i], list[i].innerText]);
+            htmlElementList.push([list[i], getInnerText(list[i])]);
         }
     });
 
@@ -33,7 +50,7 @@
             var html = domElement[1];
             var reset = false;
             symbolsList.forEach(function (symbol) {
-                var re = new RegExp('\\b' + symbol + '\\b','i');
+                var re = new RegExp('\\b' + symbol + '\\b', 'i');
                 var found = re.exec(html);
                 if (found && found.length) {
                     if (toLoadList.indexOf(symbol) === -1) {
@@ -43,8 +60,8 @@
                     reset = true;
                 }
             });
-            if( reset ){
-                domElement[0].innerHTML = domElement[0].innerText;
+            if (reset) {
+                domElement[0].innerHTML = getInnerText(domElement[0]);
             }
         });
 
@@ -61,17 +78,16 @@
 
     }
 
-    function drawLink( name, href ){
-        return '<a style="display:inline-block; background:#00ccff;color:#fff;" href="' + href + '"> -@NSAO '+ name +'- </a> ';
+    function drawLink(name, href) {
+        return '<a style="display:inline-block; background:#00ccff;color:#fff; padding: 0.38em 0.61em;" href="' + href + '"> #' + name + ' </a> ';
     }
 
     function drawItem(itemData, element) {
 
         var old_html = element.innerHTML;
         var name = itemData.name;
-        var new_html = old_html.replace(new RegExp("\\b"+name+"\\b","i"), drawLink(name,host + '/' + name ));
+        var new_html = old_html.replace(new RegExp("\\b" + name + "\\b", "i"), drawLink(name, host + '/' + name));
         element.innerHTML = new_html;
-        console.log( element, "old", old_html,"new", new_html)
 
     }
 
@@ -79,7 +95,7 @@
 
         var old_html = element.innerHTML;
         var name = tagData.name;
-        var new_html = old_html.replace(new RegExp("\\b"+name+"\\b","i"), drawLink(name,host + '/tags/' + name ) );
+        var new_html = old_html.replace(new RegExp("\\b" + name + "\\b", "i"), drawLink(name, host + '/tags/' + name));
         element.innerHTML = new_html;
 
     }
